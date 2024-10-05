e098f6c570f36f9b3ead6876a071d0278e269d05
Bonar Sitorus <87517066+BonarSs@users.noreply.github.com>
2024-10-06 01:51:26 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -23,7 +23,6 @@ tab1, tab2, tab3 = st.tabs(["Tren Jumlah Rental Sepeda", "Faktor yang Mempengaru
 
 with tab1:
     st.subheader("Tren Jumlah Rental Sepeda tiap Bulan Periode 2011-2012")
-    # Membuat diagram garis
     plt.figure(figsize=(8, 6))
     sns.regplot(x=monthly_df.index, y=monthly_df['cnt_sum'], marker='o', color='b', line_kws={"color": "red"})
     sns.lineplot(x=monthly_df.index, y=monthly_df['cnt_sum'], marker='o', label='Cnt Sum')
@@ -33,7 +32,6 @@ with tab1:
     plt.ylabel('Count')
     plt.xticks(ticks=monthly_df.index)
 
-    # Menampilkan grafik di Streamlit
     st.pyplot(plt)
     
     st.write("Grafik diatas adalah Jumlah Rental Sepeda Tiap Bulan, sumbu X menunjukkan bulan dan sumbu Y menunjukkan jumlah rental. Terlihat bahwa jumlah penggunaan sepeda meningkat dari awal tahun 2011 hingga pada akhir 2012")
@@ -79,7 +77,6 @@ with tab2:
     upperbone = 16 * x
     lowerbone = upperbone - 16
 
-    # Membuat diagram batang
     plt.figure(figsize=(10, 6))
     sns.barplot(x="dteday_", y='cnt_mean', hue='workingday_', data=weekly_df[lowerbone: upperbone])
 
@@ -91,7 +88,6 @@ with tab2:
 
     plt.tight_layout()
 
-    # Menampilkan grafik di Streamlit
     st.pyplot(plt)
 
     st.write("Grafik di atas menunjukkan rata-rata jumlah rental sepeda tiap bulan, sumbu x menunjukkan bulan sedangkan sumbu y menunjukkan rata-rata jumlah rental.")
@@ -101,7 +97,6 @@ with tab2:
 
     st.subheader("Jumlah Rental Sepeda Pada Jam-Jam Tertentu di Hari Kerja dan Non Hari Kerja")
 
-    # Menambahkan diagram kedua
     workingday_1 = hour_in_day_df[hour_in_day_df["workingday_"] == 1]
     workingday_0 = hour_in_day_df[hour_in_day_df["workingday_"] == 0]
 
@@ -113,20 +108,18 @@ with tab2:
     plt.title('Average Count per Hour Grouped by Working Day')
     plt.xlabel('Average Count (cnt_mean)')
     plt.ylabel('Hour of Day (hr)')
-    plt.axhline(0, color='black', linewidth=3)  # Garis tengah
-    plt.legend()  # Menampilkan legenda
+    plt.axhline(0, color='black', linewidth=3)  
+    plt.legend()  
     plt.tight_layout()
     
-    # Menampilkan grafik kedua di Streamlit
     st.pyplot(plt)
     
     st.write("Sumbu x pada diagram diatas menunjukkan jam rental dan sumbu Y menunjukkan rerata jumlah rental. Dapat dilihat bahwa pada hari kerja terjadi kenaikan jumlah rental pada pagi hari dan sore hari. Sedangkan pada hari non kerja, jumlah rental tertinggi terjadi pada siang hari")
-    st.markdown("<br>", unsafe_allow_html=True)  # Jarak antar grafik
+    st.markdown("<br>", unsafe_allow_html=True)  
 
     #============Diagram Ketiga======================================
 
     st.subheader("Jumlah Rental Sepeda Berdasarkan Musim")
-    # Menambahkan diagram ketiga
     plt.figure(figsize=(10, 6))
     sns.barplot(x="season_", y='cnt_mean', hue='yr_', data=seasonal_df, palette='dark')
     plt.title('Average Count Grouped by Season')
@@ -134,7 +127,6 @@ with tab2:
     plt.ylabel('Average Count')
     plt.tight_layout()
     
-    # Menampilkan grafik ketiga di Streamlit
     st.pyplot(plt)
 
     st.write("Terlihat bahwa merental sepeda bukan hal yang menarik pada springer season. (1:springer, 2:summer, 3:fall, 4:winter)")
@@ -142,7 +134,6 @@ with tab2:
 with tab3:
     st.subheader("Jumlah Rental Sepeda Berdasarkan Cuaca")
 
-    # Menambahkan diagram baru ke dalam tab 2
     plt.figure(figsize=(10, 6))
     sns.barplot(x='weathersit_', y='cnt_mean', data=weather_cnt)
 
